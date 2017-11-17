7cea2ba3201b74bc93fbb1b37c9f4ef4e45290a2
DanBrennan33 <DanBrennan33@hotmail.com>
2017-11-17 00:21:46 -0500

    Completed project for Student Database Application.

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,10 +1,107 @@
 package studentdatabaseapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String firstName;
+	private String lastName;
+	private int academicYear;
+	private int studentID;
+	private ArrayList<String> courses = new ArrayList<String>();
+	private int tuitionBalance = 0;
+	private static int courseCost = 600;
+	private static int id = 1000;
+	
+	// Constructor: Prompt to enter name and year for each student
+	public Student(Scanner input) {
+		System.out.print("Enter student first name: ");
+		this.firstName = input.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = input.nextLine();
+		
+		System.out.print("Enter student academic year: ");
+		this.academicYear = Integer.parseInt(input.nextLine());
+				
+		id++;
+		setStudentID();
 	}
 
+	// Generate student 5-digit unique ID
+	private int setStudentID() {
+		// Academic year + static id
+		return studentID = academicYear * 10000 + id;
+	}
+	
+	/*
+	 * Student can enroll in following courses (Each course is $600):
+	 * History 101
+	 * Math 101
+	 * English 101
+	 * Chemistry 101
+	 * Computer Science 101
+	 */
+	public void enroll(Scanner input) {
+		HashMap<Integer, String> coursesMap = new HashMap<Integer, String>();
+		coursesMap.put(1, "History 101");
+		coursesMap.put(2, "Math 101");
+		coursesMap.put(3, "English 101");
+		coursesMap.put(4, "Chemistry 101");
+		coursesMap.put(5, "Computer Science 101");
+		
+		System.out.println("\nChoose course enrollment (Q to quit)");
+		System.out.println("1 - History 101" +
+						   "\n2 - Math 101" +
+						   "\n3 - English 101" +
+						   "\n4 - Chemistry 101" +
+						   "\n5 - Computer Science 101");
+						   
+		while (true) {
+			System.out.print("Enter course code to enroll: ");
+			String courseCode = input.nextLine();
+			if (courseCode.equals("Q")) 
+				break;
+			courses.add(coursesMap.get(Integer.parseInt(courseCode)));
+			tuitionBalance += courseCost;
+		}
+		
+		for (String item : courses) {
+			System.out.println(item);
+		}
+	}
+	
+	// Student can view their balance and pay tuition
+	public void viewBalance() {
+		System.out.println("Student Tuition Balance: $" + tuitionBalance + " remaining.");
+	}
+	
+	// Pay Tuition
+	public void payTuition(int payment) {
+		tuitionBalance -= payment;
+		if (tuitionBalance > 0) {
+			viewBalance();
+		} else if (tuitionBalance < 0) {
+			System.out.println("Refund Amount: $" + tuitionBalance * -1 + " to be paid back to student.");
+		} else {
+			System.out.println("Student tuition paid off in full.");
+		}
+	}
+	
+	/*
+	 * View student status:
+	 * Name
+	 * ID
+	 * Courses enrolled
+	 * Tuition balance
+	 */
+	public void showStudentInfo() {
+		System.out.println("Student: " + firstName + " " + lastName);
+		System.out.println("Student ID: " + studentID);
+		System.out.println("Courses Enrolled: ");
+		for (String course : courses) 
+			System.out.println(course);
+		viewBalance();   
+	}
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,5 +1,49 @@
 package studentdatabaseapp;
 
-public class StudentDatabaseApp {
+import java.util.Scanner;
 
+public class StudentDatabaseApp {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		// Ask how many new students will be added to database
+		System.out.print("Enter number of students to enroll: ");
+		int numOfStudents = Integer.parseInt(input.nextLine());
+		Student[] students = new Student[numOfStudents];
+		System.out.println("");
+		
+		for (int i = 0; i < numOfStudents; i++) {
+			students[i] = new Student(input);
+			
+			students[i].enroll(input);
+
+			students[i].viewBalance();
+
+			while (true) {
+				System.out.print("Make a tuition payment (Yes or No): ");
+				String paymentDecision = input.nextLine();
+				if (paymentDecision.equals("Yes")) {
+					System.out.print("Payment amount: $");
+					String paymentAmount = input.nextLine();
+					students[i].payTuition(Integer.parseInt(paymentAmount));
+					break;
+				} else 
+					break;
+			}
+		}
+		
+		while (true) {
+			System.out.print("Show specific student information (Q to quit): ");
+			String studentNum = input.nextLine();
+			if (studentNum.equals("Q"))
+				break;
+			else {
+				System.out.println("");
+				students[Integer.parseInt(studentNum)].showStudentInfo();
+			}
+		}
+			
+		input.close();
+	}
 }
